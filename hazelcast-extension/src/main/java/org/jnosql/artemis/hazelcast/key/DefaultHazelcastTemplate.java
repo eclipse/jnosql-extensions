16159d341689edb2d94c387e72e4df4c1fee5ee5
Otavio Santana <otaviopolianasantana@gmail.com>
2017-12-08 13:38:10 -0300

    creates hazelcast default template

---
+++ hazelcast-extension/src/main/java/org/jnosql/artemis/hazelcast/key/DefaultHazelcastTemplate.java
@@ -0,0 +1,71 @@
+/*
+ *  Copyright (c) 2017 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.jnosql.artemis.hazelcast.key;
+
+import com.hazelcast.query.Predicate;
+import org.jnosql.artemis.key.AbstractKeyValueTemplate;
+import org.jnosql.artemis.key.KeyValueEntityConverter;
+import org.jnosql.artemis.key.KeyValueWorkflow;
+import org.jnosql.diana.api.key.BucketManager;
+import org.jnosql.diana.hazelcast.key.HazelcastBucketManager;
+
+import javax.enterprise.inject.Instance;
+import javax.inject.Inject;
+import java.util.Collection;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+class DefaultHazelcastTemplate extends AbstractKeyValueTemplate implements HazelcastTemplate {
+
+    @Inject
+    private Instance<HazelcastBucketManager> manager;
+
+    @Inject
+    private KeyValueWorkflow flow;
+
+    @Inject
+    private KeyValueEntityConverter converter;
+
+
+    @Override
+    public <T> Collection<T> query(String query) throws NullPointerException {
+        return manager.get().query(query).stream().map(v -> (T) v.get()).collect(Collectors.toList());
+    }
+
+    @Override
+    public <T> Collection<T> query(String query, Map<String, Object> params) throws NullPointerException {
+        return manager.get().query(query, params).stream().map(v -> (T) v.get()).collect(Collectors.toList());
+    }
+
+    @Override
+    public <K, V> Collection<V> query(Predicate<K, V> predicate) throws NullPointerException {
+        return manager.get().query(predicate).stream().map(v -> (V) v.get()).collect(Collectors.toList());
+    }
+
+    @Override
+    protected KeyValueEntityConverter getConverter() {
+        return converter;
+    }
+
+    @Override
+    protected BucketManager getManager() {
+        return manager.get();
+    }
+
+    @Override
+    protected KeyValueWorkflow getFlow() {
+        return flow;
+    }
+}
--- hazelcast-extension/src/main/java/org/jnosql/artemis/hazelcast/key/HazelcastTemplate.java
+++ hazelcast-extension/src/main/java/org/jnosql/artemis/hazelcast/key/HazelcastTemplate.java
@@ -14,10 +14,48 @@
  */
 package org.jnosql.artemis.hazelcast.key;
 
+import com.hazelcast.query.Predicate;
 import org.jnosql.artemis.key.KeyValueTemplate;
+
+import java.util.Collection;
+import java.util.Map;
 
 /**
  * A template layer to Hazelcast key-value type
  */
 public interface HazelcastTemplate extends KeyValueTemplate {
+
+    /**
+     * Executes hazelcast query
+     *
+     * @param <T>   the entity type
+     * @param query the query
+     * @return the result query
+     * @throws NullPointerException when there is null query
+     */
+    <T> Collection<T> query(String query) throws NullPointerException;
+
+    /**
+     * Executes hazelcast query with named query.
+     * E.g.:  bucketManager.query("name = :name", singletonMap("name", "Matrix"))
+     *
+     * @param query  the query
+     * @param <T>    the entity type
+     * @param params the params to bind
+     * @return the result query
+     * @throws NullPointerException when there is null query
+     */
+    <T> Collection<T> query(String query, Map<String, Object> params) throws NullPointerException;
+
+    /**
+     * Executes hazelcast query
+     *
+     * @param predicate the hazelcast predicate
+     * @param <K>       the key type
+     * @param <V>       the value type
+     * @return the result query
+     * @throws NullPointerException when there is null predicate
+     */
+    <K, V> Collection<V> query(Predicate<K, V> predicate) throws NullPointerException;
+
 }
