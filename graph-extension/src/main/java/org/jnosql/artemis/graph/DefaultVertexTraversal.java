9f7a62d91f47fab0b8360be082f408798a03fe8e
Otavio Santana <otaviopolianasantana@gmail.com>
2017-12-23 01:50:12 -0300

    defines until vertex traversal

--- graph-extension/src/main/java/org/jnosql/artemis/graph/DefaultVertexTraversal.java
+++ graph-extension/src/main/java/org/jnosql/artemis/graph/DefaultVertexTraversal.java
@@ -127,7 +127,7 @@ class DefaultVertexTraversal extends AbstractVertexTraversal implements VertexTr
 
     @Override
     public VertexRepeatTraversal repeat() {
-        return null;
+        return new DefaultVertexRepeatTraversal(supplier, flow, converter);
     }
 
     @Override
