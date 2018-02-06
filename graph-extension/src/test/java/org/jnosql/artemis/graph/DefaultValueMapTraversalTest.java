ca2fe758c3fe0ff8cebc4ddc7864e86b07a3c3d7
Otavio Santana <otaviopolianasantana@gmail.com>
2018-02-06 21:41:41 -0200

    adds map test

--- graph-extension/src/test/java/org/jnosql/artemis/graph/DefaultValueMapTraversalTest.java
+++ graph-extension/src/test/java/org/jnosql/artemis/graph/DefaultValueMapTraversalTest.java
@@ -14,8 +14,39 @@
  */
 package org.jnosql.artemis.graph;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.hamcrest.Matchers;
+import org.jnosql.artemis.graph.cdi.CDIExtension;
+import org.jnosql.artemis.graph.model.Person;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-class DefaultValueMapTraversalTest {
+import java.util.List;
+import java.util.stream.Collectors;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(CDIExtension.class)
+class DefaultValueMapTraversalTest extends AbstractTraversalTest {
+
+
+    @Test
+    public void shouldCount() {
+        long count = graphTemplate.getTraversalVertex()
+                .hasLabel(Person.class).valueMap("name").count();
+        assertEquals(3L, count);
+    }
+
+
+    @Test
+    public void shouldReturnMapValues() {
+        List<String> names = graphTemplate.getTraversalVertex()
+                .hasLabel(Person.class).valueMap("name")
+                .stream()
+                .map(m -> m.getOrDefault("name", "").toString()).collect(Collectors.toList());
+
+
+        assertThat(names, Matchers.containsInAnyOrder("Poliana", "Otavio", "Paulo"));
+    }
 
 }
