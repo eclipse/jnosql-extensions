76f1da35dbbf5ffa5b72d1e6944b65185faa1c6c
Otavio Santana <otaviopolianasantana@gmail.com>
2018-02-26 18:22:25 -0300

    fixes Property class

--- graph-extension/src/main/java/org/jnosql/artemis/graph/EdgeEntity.java
+++ graph-extension/src/main/java/org/jnosql/artemis/graph/EdgeEntity.java
@@ -15,6 +15,7 @@
 package org.jnosql.artemis.graph;
 
 import org.apache.tinkerpop.gremlin.structure.Edge;
+import org.apache.tinkerpop.gremlin.structure.Property;
 import org.jnosql.diana.api.Value;
 
 import java.util.List;
