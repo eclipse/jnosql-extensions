863038e82931f17d9c01e88a06842b103650ef35
Otavio Santana <otaviopolianasantana@gmail.com>
2018-04-02 20:38:24 -0300

    changes the converter

--- graph-extension/src/main/java/org/jnosql/artemis/graph/DefaultTraversalGraphConverter.java
+++ graph-extension/src/main/java/org/jnosql/artemis/graph/DefaultTraversalGraphConverter.java
@@ -14,10 +14,38 @@
  */
 package org.jnosql.artemis.graph;
 
+import static java.util.Objects.requireNonNull;
+import static java.util.stream.Collectors.toList;
+import static org.jnosql.artemis.reflection.FieldType.EMBEDDED;
+
+import java.lang.reflect.Field;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
+import org.apache.tinkerpop.gremlin.structure.Edge;
 import org.apache.tinkerpop.gremlin.structure.Graph;
+import org.apache.tinkerpop.gremlin.structure.Property;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.jnosql.artemis.AttributeConverter;
 import org.jnosql.artemis.Converters;
+import org.jnosql.artemis.EntityNotFoundException;
+import org.jnosql.artemis.reflection.ClassRepresentation;
 import org.jnosql.artemis.reflection.ClassRepresentations;
+import org.jnosql.artemis.reflection.FieldRepresentation;
 import org.jnosql.artemis.reflection.Reflections;
+import org.jnosql.diana.api.Value;
+
+import javax.enterprise.inject.Instance;
+import javax.inject.Inject;
 
 /**
  * A default implementation using DefaultTraversalGraphConverter
@@ -26,23 +54,57 @@ import org.jnosql.artemis.reflection.Reflections;
 class DefaultTraversalGraphConverter extends AbstractGraphConverter implements GraphConverter {
 
 
+    @Inject
+    private ClassRepresentations classRepresentations;
+
+    @Inject
+    private Reflections reflections;
+
+    @Inject
+    private Converters converters;
+
+    @Inject
+    private Instance<Graph> graph;
+
+
     @Override
     protected ClassRepresentations getClassRepresentations() {
-        return null;
+        return classRepresentations;
     }
 
     @Override
     protected Reflections getReflections() {
-        return null;
+        return reflections;
     }
 
     @Override
     protected Converters getConverters() {
-        return null;
+        return converters;
     }
 
     @Override
     protected Graph getGraph() {
-        return null;
+        return graph.get();
     }
+
+    @Override
+    public <T> Vertex toVertex(T entity) {
+        requireNonNull(entity, "entity is required");
+
+        ClassRepresentation representation = getClassRepresentations().get(entity.getClass());
+        String label = representation.getName();
+
+        List<FieldGraph> fields = representation.getFields().stream()
+                .map(f -> to(f, entity))
+                .filter(FieldGraph::isNotEmpty).collect(toList());
+
+        final Property id = fields.stream()
+                .filter(FieldGraph::isId)
+                .findFirst()
+                .map(i -> i.toElement(getConverters()))
+                .orElseThrow(() -> new IllegalArgumentException("Entity has not a valid Id"));
+        return graph.get().traversal().V(id.value()).next();
+
+    }
+
 }
