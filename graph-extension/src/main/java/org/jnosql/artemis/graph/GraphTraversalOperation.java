936d0acca55a147075a506a5a6bc3ad0303a745b
Otavio Santana <otaviopolianasantana@gmail.com>
2018-04-02 21:01:26 -0300

    fixes operation annotation

--- graph-extension/src/main/java/org/jnosql/artemis/graph/GraphTraversalOperation.java
+++ graph-extension/src/main/java/org/jnosql/artemis/graph/GraphTraversalOperation.java
@@ -18,11 +18,11 @@ import javax.inject.Qualifier;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Target({METHOD, TYPE})
+@Target({FIELD, TYPE})
 @Retention(RUNTIME)
 @Qualifier
 /**
