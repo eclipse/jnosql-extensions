c1bb8c4b40e3fff7a8c604a8857ae1810718f36c
Otavio Santana <otaviopolianasantana@gmail.com>
2018-04-11 19:17:34 -0300

    fixes construtoc

--- graph-extension/src/main/java/org/jnosql/artemis/graph/GraphTraversalSourceOperation.java
+++ graph-extension/src/main/java/org/jnosql/artemis/graph/GraphTraversalSourceOperation.java
@@ -19,10 +19,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Target({FIELD, TYPE})
+@Target({FIELD, TYPE, PARAMETER})
 @Retention(RUNTIME)
 @Qualifier
 /**
