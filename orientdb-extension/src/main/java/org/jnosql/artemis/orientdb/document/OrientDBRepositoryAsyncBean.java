b7a8233acca891fbedb313051bc99f7cc2373833
Otavio Santana <otaviopolianasantana@gmail.com>
2019-02-26 16:08:40 -0300

    removes redudant code at OrientDBRepositoryAsync

--- orientdb-extension/src/main/java/org/jnosql/artemis/orientdb/document/OrientDBRepositoryAsyncBean.java
+++ orientdb-extension/src/main/java/org/jnosql/artemis/orientdb/document/OrientDBRepositoryAsyncBean.java
@@ -16,14 +16,11 @@ package org.jnosql.artemis.orientdb.document;
 
 import org.jnosql.artemis.RepositoryAsync;
 import org.jnosql.artemis.document.DocumentRepositoryAsyncProducer;
+import org.jnosql.artemis.spi.AbstractBean;
 
-import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.spi.CreationalContext;
 import javax.enterprise.inject.Default;
-import javax.enterprise.inject.spi.Bean;
 import javax.enterprise.inject.spi.BeanManager;
-import javax.enterprise.inject.spi.InjectionPoint;
-import javax.enterprise.inject.spi.PassivationCapable;
 import javax.enterprise.util.AnnotationLiteral;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Proxy;
@@ -32,11 +29,9 @@ import java.util.Collections;
 import java.util.Set;
 
 
-class OrientDBRepositoryAsyncBean implements Bean<OrientDBCrudRepositoryAsync>, PassivationCapable {
+class OrientDBRepositoryAsyncBean extends AbstractBean<OrientDBCrudRepositoryAsync> {
 
     private final Class type;
-
-    private final BeanManager beanManager;
 
     private final Set<Type> types;
 
@@ -44,24 +39,14 @@ class OrientDBRepositoryAsyncBean implements Bean<OrientDBCrudRepositoryAsync>, 
     });
 
     OrientDBRepositoryAsyncBean(Class type, BeanManager beanManager) {
+        super(beanManager);
         this.type = type;
-        this.beanManager = beanManager;
         this.types = Collections.singleton(type);
     }
 
     @Override
     public Class<?> getBeanClass() {
         return type;
-    }
-
-    @Override
-    public Set<InjectionPoint> getInjectionPoints() {
-        return Collections.emptySet();
-    }
-
-    @Override
-    public boolean isNullable() {
-        return false;
     }
 
     @Override
@@ -77,18 +62,6 @@ class OrientDBRepositoryAsyncBean implements Bean<OrientDBCrudRepositoryAsync>, 
                 handler);
     }
 
-
-    private <T> T getInstance(Class<T> clazz) {
-        Bean<T> bean = (Bean<T>) beanManager.getBeans(clazz).iterator().next();
-        CreationalContext<T> ctx = beanManager.createCreationalContext(bean);
-        return (T) beanManager.getReference(bean, clazz, ctx);
-    }
-
-    @Override
-    public void destroy(OrientDBCrudRepositoryAsync instance, CreationalContext<OrientDBCrudRepositoryAsync> creationalContext) {
-
-    }
-
     @Override
     public Set<Type> getTypes() {
         return types;
@@ -97,26 +70,6 @@ class OrientDBRepositoryAsyncBean implements Bean<OrientDBCrudRepositoryAsync>, 
     @Override
     public Set<Annotation> getQualifiers() {
         return qualifiers;
-    }
-
-    @Override
-    public Class<? extends Annotation> getScope() {
-        return ApplicationScoped.class;
-    }
-
-    @Override
-    public String getName() {
-        return null;
-    }
-
-    @Override
-    public Set<Class<? extends Annotation>> getStereotypes() {
-        return Collections.emptySet();
-    }
-
-    @Override
-    public boolean isAlternative() {
-        return false;
     }
 
     @Override
