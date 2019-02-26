eef32137c60721a101084db97aa1a9d82d4b94cf
Otavio Santana <otaviopolianasantana@gmail.com>
2019-02-26 16:06:34 -0300

    removes redudant code at OrientDBRepository

--- orientdb-extension/src/main/java/org/jnosql/artemis/orientdb/document/OrientDBRepositoryBean.java
+++ orientdb-extension/src/main/java/org/jnosql/artemis/orientdb/document/OrientDBRepositoryBean.java
@@ -16,14 +16,11 @@ package org.jnosql.artemis.orientdb.document;
 
 import org.jnosql.artemis.Repository;
 import org.jnosql.artemis.document.DocumentRepositoryProducer;
+import org.jnosql.artemis.spi.AbstractBean;
 
-import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.spi.CreationalContext;
 import javax.enterprise.inject.Default;
-import javax.enterprise.inject.spi.Bean;
 import javax.enterprise.inject.spi.BeanManager;
-import javax.enterprise.inject.spi.InjectionPoint;
-import javax.enterprise.inject.spi.PassivationCapable;
 import javax.enterprise.util.AnnotationLiteral;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Proxy;
@@ -32,11 +29,9 @@ import java.util.Collections;
 import java.util.Set;
 
 
-class OrientDBRepositoryBean implements Bean<OrientDBCrudRepository>, PassivationCapable {
+class OrientDBRepositoryBean extends AbstractBean<OrientDBCrudRepository> {
 
     private final Class type;
-
-    private final BeanManager beanManager;
 
     private final Set<Type> types;
 
@@ -44,8 +39,8 @@ class OrientDBRepositoryBean implements Bean<OrientDBCrudRepository>, Passivatio
     });
 
     OrientDBRepositoryBean(Class type, BeanManager beanManager) {
+        super(beanManager);
         this.type = type;
-        this.beanManager = beanManager;
         this.types = Collections.singleton(type);
     }
 
@@ -54,15 +49,6 @@ class OrientDBRepositoryBean implements Bean<OrientDBCrudRepository>, Passivatio
         return type;
     }
 
-    @Override
-    public Set<InjectionPoint> getInjectionPoints() {
-        return Collections.emptySet();
-    }
-
-    @Override
-    public boolean isNullable() {
-        return false;
-    }
 
     @Override
     public OrientDBCrudRepository create(CreationalContext<OrientDBCrudRepository> creationalContext) {
@@ -77,18 +63,6 @@ class OrientDBRepositoryBean implements Bean<OrientDBCrudRepository>, Passivatio
     }
 
 
-    private <T> T getInstance(Class<T> clazz) {
-        Bean<T> bean = (Bean<T>) beanManager.getBeans(clazz).iterator().next();
-        CreationalContext<T> ctx = beanManager.createCreationalContext(bean);
-        return (T) beanManager.getReference(bean, clazz, ctx);
-    }
-
-
-    @Override
-    public void destroy(OrientDBCrudRepository instance, CreationalContext<OrientDBCrudRepository> creationalContext) {
-
-    }
-
     @Override
     public Set<Type> getTypes() {
         return types;
@@ -97,21 +71,6 @@ class OrientDBRepositoryBean implements Bean<OrientDBCrudRepository>, Passivatio
     @Override
     public Set<Annotation> getQualifiers() {
         return qualifiers;
-    }
-
-    @Override
-    public Class<? extends Annotation> getScope() {
-        return ApplicationScoped.class;
-    }
-
-    @Override
-    public String getName() {
-        return null;
-    }
-
-    @Override
-    public Set<Class<? extends Annotation>> getStereotypes() {
-        return Collections.emptySet();
     }
 
     @Override
