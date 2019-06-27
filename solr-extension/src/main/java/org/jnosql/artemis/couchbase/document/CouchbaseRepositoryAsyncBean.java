d952befefe79eac6adfbc6d5e62685f2c73fd08e
Otavio Santana <otaviopolianasantana@gmail.com>
2019-06-27 20:21:44 -0300

    adds solr extension project

---
+++ solr-extension/src/main/java/org/jnosql/artemis/couchbase/document/CouchbaseRepositoryAsyncBean.java
@@ -0,0 +1,79 @@
+/*
+ *  Copyright (c) 2017 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.jnosql.artemis.couchbase.document;
+
+import jakarta.nosql.mapping.RepositoryAsync;
+import jakarta.nosql.mapping.document.DocumentRepositoryAsyncProducer;
+import org.jnosql.artemis.spi.AbstractBean;
+
+import javax.enterprise.context.spi.CreationalContext;
+import javax.enterprise.inject.Default;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.util.AnnotationLiteral;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Proxy;
+import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.Set;
+
+
+class CouchbaseRepositoryAsyncBean extends AbstractBean<CouchbaseRepositoryAsync> {
+
+    private final Class type;
+
+
+    private final Set<Type> types;
+
+    private final Set<Annotation> qualifiers = Collections.singleton(new AnnotationLiteral<Default>() {
+    });
+
+    CouchbaseRepositoryAsyncBean(Class type, BeanManager beanManager) {
+        super(beanManager);
+        this.type = type;
+        this.types = Collections.singleton(type);
+    }
+
+    @Override
+    public Class<?> getBeanClass() {
+        return type;
+    }
+
+    @Override
+    public CouchbaseRepositoryAsync create(CreationalContext<CouchbaseRepositoryAsync> creationalContext) {
+        CouchbaseTemplateAsync templateAsync = getInstance(CouchbaseTemplateAsync.class);
+        DocumentRepositoryAsyncProducer producer = getInstance(DocumentRepositoryAsyncProducer.class);
+        RepositoryAsync<?,?> repositoryAsync = producer.get((Class<RepositoryAsync<Object, Object>>) type, templateAsync);
+        CouchbaseRepositoryAsyncProxy handler = new CouchbaseRepositoryAsyncProxy(templateAsync, repositoryAsync);
+        return (CouchbaseRepositoryAsync) Proxy.newProxyInstance(type.getClassLoader(),
+                new Class[]{type},
+                handler);
+    }
+
+    @Override
+    public Set<Type> getTypes() {
+        return types;
+    }
+
+    @Override
+    public Set<Annotation> getQualifiers() {
+        return qualifiers;
+    }
+
+    @Override
+    public String getId() {
+        return type.getName() + "Async@couchbase";
+    }
+
+}
---
+++ solr-extension/src/main/java/org/jnosql/artemis/couchbase/document/CouchbaseRepositoryAsyncProxy.java
@@ -0,0 +1,69 @@
+/*
+ *  Copyright (c) 2017 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.jnosql.artemis.couchbase.document;
+
+
+import com.couchbase.client.java.document.json.JsonObject;
+import jakarta.nosql.mapping.RepositoryAsync;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.Objects;
+import java.util.function.Consumer;
+
+import static org.jnosql.artemis.couchbase.document.JsonObjectUtil.getParams;
+
+class CouchbaseRepositoryAsyncProxy<T>  implements InvocationHandler {
+
+    private static final Consumer NOOP = t -> {
+    };
+
+
+    private final CouchbaseTemplateAsync template;
+
+
+    private final RepositoryAsync<?, ?> repositoryAsync;
+
+
+    CouchbaseRepositoryAsyncProxy(CouchbaseTemplateAsync template, RepositoryAsync<?, ?> repositoryAsync) {
+        this.template = template;
+        this.repositoryAsync = repositoryAsync;
+    }
+
+
+
+    @Override
+    public Object invoke(Object instance, Method method, Object[] args) throws Throwable {
+
+        N1QL n1QL = method.getAnnotation(N1QL.class);
+        if (Objects.nonNull(n1QL)) {
+            Consumer callBack = NOOP;
+            if (Consumer.class.isInstance(args[args.length - 1])) {
+                callBack = Consumer.class.cast(args[args.length - 1]);
+            }
+
+            JsonObject params = getParams(args, method);
+            if (params.isEmpty()) {
+                template.n1qlQuery(n1QL.value(), callBack);
+                return Void.class;
+            } else {
+                template.n1qlQuery(n1QL.value(), params, callBack);
+                return Void.class;
+            }
+        }
+        return method.invoke(repositoryAsync, args);
+    }
+
+}
