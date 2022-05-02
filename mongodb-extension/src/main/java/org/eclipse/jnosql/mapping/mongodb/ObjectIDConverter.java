c4e4bbdbc4ac5ea937716a689c334c0d21113256
Otavio Santana <otaviopolianasantana@gmail.com>
2022-05-02 10:21:29 +0100

    Create ObjectID converter
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- mongodb-extension/src/main/java/org/eclipse/jnosql/mapping/mongodb/ObjectIDConverter.java
+++ mongodb-extension/src/main/java/org/eclipse/jnosql/mapping/mongodb/ObjectIDConverter.java
@@ -1,2 +1,31 @@
-package org.eclipse.jnosql.mapping.mongodb;public class ObjectIDConverter {
+/*
+ *  Copyright (c) 2022 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+
+package org.eclipse.jnosql.mapping.mongodb;
+
+import jakarta.nosql.mapping.AttributeConverter;
+import org.bson.types.ObjectId;
+
+public class ObjectIDConverter implements AttributeConverter<String, ObjectId> {
+    @Override
+    public ObjectId convertToDatabaseColumn(String attribute) {
+        return null;
+    }
+
+    @Override
+    public String convertToEntityAttribute(ObjectId dbData) {
+        return null;
+    }
 }
