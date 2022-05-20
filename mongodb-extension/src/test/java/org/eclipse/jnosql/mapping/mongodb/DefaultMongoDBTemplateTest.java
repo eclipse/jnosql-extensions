afb6182fd8f84078dfcd26cd76c34503ce32dd1b
Otavio Santana <otaviopolianasantana@gmail.com>
2022-05-20 16:15:05 +0100

    Add Delete test on mongodb template
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- mongodb-extension/src/test/java/org/eclipse/jnosql/mapping/mongodb/DefaultMongoDBTemplateTest.java
+++ mongodb-extension/src/test/java/org/eclipse/jnosql/mapping/mongodb/DefaultMongoDBTemplateTest.java
@@ -14,19 +14,25 @@
  */
 package org.eclipse.jnosql.mapping.mongodb;
 
+import com.mongodb.client.model.Filters;
 import jakarta.nosql.mapping.Converters;
 import jakarta.nosql.mapping.document.DocumentEntityConverter;
 import jakarta.nosql.mapping.document.DocumentEventPersistManager;
 import jakarta.nosql.mapping.document.DocumentWorkflow;
+import org.bson.conversions.Bson;
 import org.eclipse.jnosql.communication.mongodb.document.MongoDBDocumentCollectionManager;
 import org.eclipse.jnosql.mapping.reflection.ClassMappings;
 import org.eclipse.jnosql.mapping.test.CDIExtension;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 
+import static com.mongodb.client.model.Filters.eq;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.nullable;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -58,5 +64,31 @@ class DefaultMongoDBTemplateTest {
         Instance instance = mock(Instance.class);
         when(instance.get()).thenReturn(manager);
         template = new DefaultMongoDBTemplate(instance, converter, flow, mappings, converters, persistManager);
+    }
+
+    @Test
+    public void shouldReturnErrorOnDeleteMethod() {
+        assertThrows(NullPointerException.class, () -> template.delete((String) null, null));
+        assertThrows(NullPointerException.class, () -> template.delete("Collection", null));
+        assertThrows(NullPointerException.class, () -> template.delete((String) null,
+                eq("name", "Poliana")));
+
+        assertThrows(NullPointerException.class, () -> template.delete(Person.class, null));
+        assertThrows(NullPointerException.class, () -> template.delete((Class<Object>) null,
+                eq("name", "Poliana")));
+    }
+
+    @Test
+    public void shouldDeleteWithCollectionName() {
+        Bson filter = eq("name", "Poliana");
+        template.delete("Person", filter);
+        Mockito.verify(manager).delete("Person", filter);
+    }
+
+    @Test
+    public void shouldDeleteWithEntity() {
+        Bson filter = eq("name", "Poliana");
+        template.delete(Person.class, filter);
+        Mockito.verify(manager).delete("Person", filter);
     }
 }
