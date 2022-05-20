07e309ce4b636f4836b8f1bae822e5861b46b8f0
Otavio Santana <otaviopolianasantana@gmail.com>
2022-05-20 16:30:20 +0100

    remove setter method on Person class
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- mongodb-extension/src/test/java/org/eclipse/jnosql/mapping/mongodb/Person.java
+++ mongodb-extension/src/test/java/org/eclipse/jnosql/mapping/mongodb/Person.java
@@ -35,18 +35,9 @@ public class Person {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public Integer getAge() {
         return age;
     }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
 
     public Person(String name, Integer age) {
         this.name = name;
