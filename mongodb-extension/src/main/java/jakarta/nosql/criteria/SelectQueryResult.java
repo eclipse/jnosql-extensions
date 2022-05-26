529cd881a48e51a2f6696ed22a695c46edcf7857
Alessandro Moscatelli <alessandro.moscatelli@live.com>
2022-05-26 11:45:33 +0200

    fix header license

--- mongodb-extension/src/main/java/jakarta/nosql/criteria/SelectQueryResult.java
+++ mongodb-extension/src/main/java/jakarta/nosql/criteria/SelectQueryResult.java
@@ -1,21 +1,16 @@
 /*
- * Copyright (c) 2022 Otavio Santana and others
+ *  Copyright (c) 2022 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
  *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v. 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0.
+ *   You may elect to redistribute this code under either of these licenses.
  *
- * This Source Code may also be made available under the following Secondary
- * Licenses when the conditions for such availability set forth in the Eclipse
- * Public License v. 2.0 are satisfied: GNU General Public License, version 2
- * with the GNU Classpath Exception which is available at
- * https://www.gnu.org/software/classpath/license.html.
+ *   Contributors:
  *
- * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
- 
- * Contributors:
- *     Alessandro Moscatelli
- *
+ *   Alessandro Moscatelli
  */
 package jakarta.nosql.criteria;
 
