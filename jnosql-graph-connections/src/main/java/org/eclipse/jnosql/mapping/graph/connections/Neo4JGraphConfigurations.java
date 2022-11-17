35a80d0aed6dddd41aa84fdc0a0c9ac1afa4c6d6
Otavio Santana <otaviopolianasantana@gmail.com>
2022-11-17 05:29:50 +0000

    update neo4jconfiguration
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ jnosql-graph-connections/src/main/java/org/eclipse/jnosql/mapping/graph/connections/Neo4JGraphConfigurations.java
@@ -0,0 +1,34 @@
+/*
+ *  Copyright (c) 2022 Eclipse Contribuitor
+ * All rights reserved. This program and the accompanying materials
+ *  and Apache License v2.0 which accompanies this distribution.
+ *  The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *  and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *    You may elect to redistribute this code under either of these licenses.
+ */
+
+package org.eclipse.jnosql.mapping.graph.connections;
+
+import java.util.function.Supplier;
+
+/**
+ * An enumeration to show the available options to connect to the Neo4J database.
+ * It implements {@link Supplier}, where its it returns the property name that might be
+ * overwritten by the system environment using Eclipse Microprofile or Jakarta Config API.
+ *
+ * @see jakarta.nosql.Settings
+ */
+public enum Neo4JGraphConfigurations implements Supplier<String> {
+
+    HOST("jnosql.neo4j.host"), USER("jnosql.neo4j.user"), PASSWORD("jnosql.neo4j.password");
+
+    private final String value;
+    Neo4JGraphConfigurations(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String get() {
+        return value;
+    }
+}
