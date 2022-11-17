4c3468cedb35002544d8862300396c85c7b1e903
Otavio Santana <otaviopolianasantana@gmail.com>
2022-11-17 05:41:48 +0000

    create arangondb documentation
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-graph-connections/src/main/java/org/eclipse/jnosql/mapping/graph/connections/ArangoDBGraphConfigurations.java
+++ jnosql-graph-connections/src/main/java/org/eclipse/jnosql/mapping/graph/connections/ArangoDBGraphConfigurations.java
@@ -11,14 +11,46 @@ package org.eclipse.jnosql.mapping.graph.connections;
 
 import java.util.function.Supplier;
 
+/**
+ * An enumeration to show the available options to connect to the ArangoDB database.
+ * It implements {@link Supplier}, where its it returns the property name that might be
+ * overwritten by the system environment using Eclipse Microprofile or Jakarta Config API.
+ *
+ * @see jakarta.nosql.Settings
+ */
 public enum ArangoDBGraphConfigurations implements Supplier<String> {
 
+    /**
+     *
+     */
     EDGE("jnosql.arangodb.graph.edge"),
+    /**
+     *
+     */
     EDGE_CONFIGURATION("jnosql.arangodb.graph.edge.configuration"),
+    /**
+     *
+     */
     VERTEX("jnosql.arangodb.graph.vertex"),
-    GRAPH("jnosql.arangodb.graph.graph");
+    /**
+     *
+     */
+    GRAPH("jnosql.arangodb.graph.graph"),
+    /**
+     * The database host.
+     */
+    HOST("jnosql.arangodb.graph.host"),
+    /**
+     * The user's credential.
+     */
+    USER("jnosql.arangodb.graph.user"),
+    /**
+     * The password's credential.
+     */
+    PASSWORD("jnosql.arangodb.graph.password");
 
     private final String value;
+
     ArangoDBGraphConfigurations(String value) {
         this.value = value;
     }
