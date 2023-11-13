e295ce7445f01df627ab6d18ba21fb15789b6e61
Maximillian Arruda <dearrudam@gmail.com>
2023-11-13 14:45:31 -0300

    fix: repository deleteAll() method changed to call getTemplate.deleteAll(Class) method;
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

--- jnosql-lite/mapping-lite-graph-test/src/test/java/org/eclipse/jnosql/lite/mapping/entities/PersonRepositoryLiteGraphTest.java
+++ jnosql-lite/mapping-lite-graph-test/src/test/java/org/eclipse/jnosql/lite/mapping/entities/PersonRepositoryLiteGraphTest.java
@@ -100,7 +100,7 @@ public class PersonRepositoryLiteGraphTest {
     public void shouldDeleteAllEntities() {
         repository.deleteAll();
 
-        verify(template).delete(eq(Person.class));
+        verify(template).deleteAll(eq(Person.class));
     }
 
     @Test
